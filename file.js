e4c9673ca742401a0be4438fba3d99e629b07ae9
Volodymyr <144222875+vdedeliuk@users.noreply.github.com>
2023-09-11 20:24:29 +0300

    2
    2

---
+++ file.js
@@ -0,0 +1,16 @@
+
+let	a = 1
+let b = "ukd"
+let c = NaN
+let d = true
+let e = "undefined"
+
+let me = {
+	name: "kun",
+	surname: "vays",
+	age: 17,
+	home: "Ukraine"
+}
+
+console.log(a, b, c, d, e)
+console.table(me)
